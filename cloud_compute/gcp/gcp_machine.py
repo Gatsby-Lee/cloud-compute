5064fc8fd1c3cd432b179a160291305b85f9974e
Gatsby Lee <thewayofthinking@gmail.com>
2019-08-25 14:45:27 -0700

    update gcp_machine_type to object

--- cloud_compute/gcp/gcp_machine.py
+++ cloud_compute/gcp/gcp_machine.py
@@ -9,11 +9,18 @@ ref: https://cloud.google.com/compute/docs/reference/rest/v1/instances/insert
 TEMPLATE_MACHINE_TYPE = 'zones/{}/machineTypes/{}'
 
 
-def get_machine_type_fullname(zone, machine_type):
-    machine_type = machine_type.strip()
-    if machine_type.startswith('custom-'):
-        _, memory = machine_type[machine_type.index('-') + 1:].split('-')
-        if int(memory) % 256:
-            raise ValueError('Memory must be a multiple of 256 MB')
+class MachineType(object):
 
-    return TEMPLATE_MACHINE_TYPE.format(zone, machine_type)
+    def __init__(self, zone, machine_type):
+
+        machine_type = machine_type.strip()
+        if machine_type.startswith('custom-'):
+            _, memory = machine_type[machine_type.index('-') + 1:].split('-')
+            if int(memory) % 256:
+                raise ValueError('Memory must be a multiple of 256 MB')
+
+        self._zone = zone
+        self._machine_type = machine_type
+
+    def get(self):
+        return TEMPLATE_MACHINE_TYPE.format(self._zone, self._machine_type)
---
+++ tests/cloud_compute/gcp/test_gcp_machine.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_machine_type_init():
+    from cloud_compute.gcp.gcp_machine import MachineType
+    m = MachineType('us-east1-a', 'n1-standard-1')
+    assert m.get() == 'zones/us-east1-a/machineTypes/n1-standard-1'
+
+
+def test_machine_type_custom_memory_exception():
+    from cloud_compute.gcp.gcp_machine import MachineType
+    with pytest.raises(ValueError):
+        MachineType('us-east1-a', 'custom-8-1023')
